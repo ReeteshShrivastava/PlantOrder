14890a384b7064ec80fc64c07029e4a8cb344b05
Alok Singh <askushw07@gmail.com>
2023-07-08 09:23:53 +0530

    Home Page Completed

--- Components/navbar.js
+++ Components/navbar.js
@@ -1,7 +1,7 @@
 export function navbar(){
     return `
         <nav>
-            <div>
+            <div id="logo>
                 <p><a href="./index.html">Green Paradise</a></p>
             </div>
             <div>
@@ -20,10 +20,37 @@ export function navbar(){
         </nav>`
 }
 
+export function reponsive_nav(){
+    return `
+    <nav>
+    <div>
+      <p><input type="text" id="item" placeholder="Search Plant" style="width: 100%;"></p>
+      <p><button id="search"><i class="fa-solid fa-magnifying-glass fa-lg"></i></button></p>
+      <p><a href=""><i class="fa-solid fa-language fa-lg"></i></a></p>
+    </div>
+    <div style="width: 100%;">
+      <div id="logo">
+        <p><a href="./index.html">Green Paradise</a></p>
+      </div>
+      <div>
+          <button id="menu"><i class="fa-solid fa-bars fa-2xl"></i></button>
+      </div>
+    </div>
+    <div id="respnav">
+        <p><a href="./SingIn.html">Login</a></p>
+        <p><a href="./productPage.html">Catlog</a></p>
+        <p><a href="">Contact</a></p>
+        <p><a href="">Sale</a></p>
+        <p><a href="">Cart</a></p>
+        <p><a href="./checkOutPage.html">Delivery & Payment</a></p>
+    </div>
+  </nav>`
+}
+
 export async function fetchdata(){
     let res  = await fetch("https://gentle-ant-handkerchief.cyclic.app/plantDetails");
     let data = await res.json();
-    // console.log(data);
+    console.log(data);
     document.getElementById("item").addEventListener("keyup",()=>{
         let val = document.getElementById("item").value;
         if(!val) return;
@@ -52,11 +79,10 @@ export const debounce = (func, delay) => {
     console.log(`fetch request made`);
     let res = await fetch(`https://gentle-ant-handkerchief.cyclic.app/plantDetails?q=${query}`);
     let data = await res.json();
-    let filteredData = data.filter(item => item.name.includes(query));
-    console.log(filteredData);
-    console.log(data);
+    console.log(data)
     let table=document.getElementById("popup").querySelector("table");
     table.innerHTML=null;
+    let item=document.getElementById("item");
     data.forEach(element => {
         let name = element.name;
         let tr=document.createElement("tr");
@@ -64,17 +90,16 @@ export const debounce = (func, delay) => {
         td.textContent=name;
         tr.append(td);
         table.append(tr);
-        let item=document.getElementById("item");
         tr.addEventListener("click",()=>{
-            console.log(td.textContent)
             item.value=td.textContent;
-            item.placeholder=td.textContent;
+            item.textContent=td.textContent;
+            localStorage.setItem("id",element.id);
             document.getElementById("popup").style.display="none";
         })
     });
     if(item.value)
     document.getElementById("popup").style.display="block";
-    else document.getElementById("item").placeholder="Search Plant";
+    else document.getElementById("popup").style.display="none";
   }
 
   export let betterFetchItems = debounce(fetchItems, 1500);
--- SCRIPTS/index.js
+++ SCRIPTS/index.js
@@ -1,6 +1,10 @@
 import * as functions from "../Components/navbar.js";
 import footer from "../Components/footer.js";
+
+
 document.getElementById("nav-bar").innerHTML=functions.navbar();
+let navbar=document.getElementById("navbar");
+navbar.innerHTML=functions.reponsive_nav();
 document.getElementById("footer").innerHTML=footer();
 
 
@@ -12,7 +16,24 @@ document.getElementById("footer").innerHTML=footer();
 document.getElementById("item").addEventListener("keyup", function (e) {
   if(e.target.value)
   functions.betterFetchItems(e.target.value);
+  else{
+    document.getElementById("popup").style.display="none";
+  }
 });
 
 
 //document.getElementById("popup").addEventListener("click",myFunction);
+
+
+
+
+  document.getElementById("menu").addEventListener("click",()=>{
+    let rnav = document.getElementById("respnav");
+    let state = rnav.style.display;
+    if(!state || state=="none"){
+      rnav.style.display="flex";
+    }
+    else{
+      rnav.style.display="none";
+    }
+  })
